46ace180b98cbb0871e355ac8643f35287faf859
meetshashank007 <meet.shashank007@gmail.com>
2017-11-25 13:57:34 +0530

    first commit

---
+++ backend/rbac/assignments.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    1 => [
+        'su',
+        'sysadmin',
+    ],
+    2 => [
+        'su',
+        'sysadmin',
+    ],
+    3 => [
+        'normal',
+        'user',
+    ],
+];
---
+++ common/config/main.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'authManager' => [
+        	'class' => 'yii\rbac\DbManager' //or use 'yii\rbac\PhpManager'
+        ],
+        'user' => [
+        	//'class' => 'mdm\admin\models\User',
+        	'identityClass' => 'mdm\admin\models\User',
+        	'loginUrl' => ['admin/user/login'],
+        ]
+    ],
+    'modules' => [
+    	'admin' => [
+    		'class' => 'mdm\admin\Module'
+    	]
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',	
+        ]
+    ]
+];
